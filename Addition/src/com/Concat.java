c4a59fe87f25b3bd83a76b072dc6285458e63356
A-Nithya <anithyanarayanan@gmail.com>
2017-11-28 10:34:32 +0100

    first commit

---
+++ Addition/src/com/Concat.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author nxf37731
+ *
+ */
+public class Concat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Object(){}.getClass().getEnclosingClass().getName());
+
+	}
+
+}
